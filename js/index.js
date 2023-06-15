683fc2139e459c342c33ea2e6003d94a1384b418
Tapishagrawal <tapishg4@gmail.com>
2023-06-16 00:34:14 +0530

    add day-2 work

---
+++ js/index.js
@@ -0,0 +1,55 @@
+// Card scroll functionality Start
+const cardContainer = document.querySelector(".card-container")
+const arrowBtns = document.querySelectorAll(".cardbtn i")
+const forstCardWidth = cardContainer.querySelector(".card").offsetWidth;
+arrowBtns.forEach(btn =>{
+    btn.addEventListener("click",()=>{
+        cardContainer.scrollLeft += btn.id === "left" ? -forstCardWidth : forstCardWidth
+    })
+})
+let isDragging = false, startX,StartScrollLeft;
+const dragStart = (e)=>{
+    isDragging  = true
+    cardContainer.classList.add("dragging")
+    startX = e.pageX;
+    StartScrollLeft = cardContainer.scrollLeft;
+}
+const dragging = (e)=>{
+    if(!isDragging) return
+    cardContainer.scrollLeft = StartScrollLeft - (e.pageX - startX)
+}
+const dragStop = ()=>{
+    isDragging  = false
+    cardContainer.classList.remove("dragging")
+}
+cardContainer.addEventListener("mousedown",dragStart)
+cardContainer.addEventListener("mousemove",dragging)
+cardContainer.addEventListener("mouseup",dragStop)
+// Card scroll functionality End
+
+//Toggle Card Like add and remove start
+const cardHeart = document.querySelectorAll(".card-img-cont > i")
+
+cardHeart.forEach((e)=>{
+    e.addEventListener("click",()=>{
+        e.classList.toggle("fa-solid")
+    })
+})
+//Toggle Card Like add and remove End
+
+// Testimonial Start
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop:true,
+});
+// Testimonial End
