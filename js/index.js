41a9a289639309229bb3673678cb2873e036ff6c
Tapishagrawal <tapishg4@gmail.com>
2023-06-16 01:05:31 +0530

    day-3 FAQs functionality part add

--- js/index.js
+++ js/index.js
@@ -53,3 +53,20 @@ var swiper = new Swiper(".mySwiper", {
     loop:true,
 });
 // Testimonial End
+
+// FAQ Start
+const faqQuestion = document.getElementsByClassName("faq-question");
+for(let i = 0; i < faqQuestion.length; i++){
+    faqQuestion[i].addEventListener("click",function(){
+        this.classList.toggle("active")
+        this.parentElement.classList.toggle("active");
+
+        var faqAns = this.nextElementSibling;
+        if(faqAns.style.display=="block"){
+            faqAns.style.display="none"
+        }else{
+            faqAns.style.display="block"
+        }
+    })
+}
+// FAQ End
